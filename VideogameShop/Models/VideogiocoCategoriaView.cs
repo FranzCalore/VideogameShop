601f99fcd3f5f97086e3cba9967a5bd4ecddf9b6
RedGuardiaN7 <artiom.str@hotmail.it>
2023-01-26 17:20:58 +0100

    Aggiunta view + controller

---
+++ VideogameShop/Models/VideogiocoCategoriaView.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace VideogameShop.Models
+{
+    public class VideogiocoCategoriaView
+    {
+        public Videogioco Videogioco { get; set; }
+
+        public List<Tipologia>? Tipologie { get; set; }
+
+    }
+}
