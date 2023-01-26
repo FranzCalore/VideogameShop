3230e65bcdd34159395fd5031458d1467e0d40d2
FranzCalore <franzkaiser1993@gmail.com>
2023-01-26 16:36:56 +0100

    Rifinito il context

--- VideogameShop/Database/VideogameContext.cs
+++ VideogameShop/Database/VideogameContext.cs
@@ -1,6 +1,15 @@
-﻿namespace VideogameShop.Database
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VideogameShop.Database
 {
-    public class VideogameContext
+    public class VideogameContext:DbContext
     {
+        public DbSet<Videogioco> Videogiochi { get; set; }
+        public DbSet<Tipologia> Tipologie { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = localhost; Initial Catalog = VideoGameDB; Integrated Security = True; Pooling = False;TrustServerCertificate=True");
+        }
     }
 }
