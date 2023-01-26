2d8f283041c231e0c3e4fa9af09e5ad4daaff0a0
RedGuardiaN7 <artiom.str@hotmail.it>
2023-01-26 16:38:52 +0100

    Creata branch modello videogioco

---
+++ VideogameShop/Models/Videogioco.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace VideogameShop.Models
+{
+    public class Videogioco
+    {
+    }
+}
