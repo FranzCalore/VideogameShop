0c5cff13fde98d0e620427a0f817db42e78ec6dd
FranzCalore <franzkaiser1993@gmail.com>
2023-01-27 12:04:28 +0100

    Completato l'API controller

--- VideogameShop/Controllers/HomeController.cs
+++ VideogameShop/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VideogameShop.Database;
+using VideogameShop.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,34 +13,38 @@ namespace VideogameShop.Controllers
     {
         // GET: api/<HomeController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get(string? search)
         {
-            return new string[] { "value1", "value2" };
+            using VideogameContext db = new();
+            List<Videogioco> ListaVideogiochi = new();
+            if(search is null || search == "")
+            {
+                ListaVideogiochi = db.Videogiochi.ToList();
+            }
+            else
+            {
+                search = search.ToLower();
+                ListaVideogiochi = db.Videogiochi.Include(V=>V.Tipologia)
+                                                 .Where(V => V.Nome.ToLower().Contains(search) ||                                         
+                                                        V.Tipologia.TipologiaNome.ToLower().Contains(search) ||
+                                                        V.Descrizione.ToLower().Contains(search))
+                                                 .ToList();
+            }
+            return Ok(ListaVideogiochi);
         }
 
         // GET api/<HomeController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            using VideogameContext db = new();
+            Videogioco videogioco = db.Videogiochi.Where(V => V.Id == id).Include(Videogioco => Videogioco.Tipologia).FirstOrDefault();
+            if (videogioco is null)
+            {
+                return NotFound("Mario, sembra che il tuo videogioco sia in un altro castello!");
+            }
+            return Ok(videogioco);
         }
 
-        // POST api/<HomeController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<HomeController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<HomeController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
