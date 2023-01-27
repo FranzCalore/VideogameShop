9bae87eca6e9c48eaf2adb02601f9755924df0c4
FranzCalore <franzkaiser1993@gmail.com>
2023-01-27 12:09:09 +0100

    Aggiunto costruttore vuoto ad acquisto

--- VideogameShop/Models/Acquisto.cs
+++ VideogameShop/Models/Acquisto.cs
@@ -14,6 +14,8 @@ namespace VideogameShop.Models
 
         public int Quantita { get; set; }
 
+        public Acquisto() { }
+
         public Acquisto(int acquistoId, DateTime dataAcquisto, int videogiocoId, int quantita)
         {
             AcquistoId = acquistoId;
