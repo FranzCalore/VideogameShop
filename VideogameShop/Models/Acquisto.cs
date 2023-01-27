bd224a22aa0f9bc3562e643aa92c46851f9933df
ZahidRamirez00 <zahidramirez.00@gmail.com>
2023-01-27 12:07:51 +0100

    Model-Acquisto.cs finito

--- VideogameShop/Models/Acquisto.cs
+++ VideogameShop/Models/Acquisto.cs
@@ -1,6 +1,25 @@
-﻿namespace VideogameShop.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using VideogameShop.Models;
+
+namespace VideogameShop.Models
 {
+    [Table("Acquisto")]
     public class Acquisto
     {
+        public int AcquistoId { get; set; }
+
+        public DateTime DataAcquisto { get; set; }
+        public int VideogiocoId { get; set; }
+
+        public int Quantita { get; set; }
+
+        public Acquisto(int acquistoId, DateTime dataAcquisto, int videogiocoId, int quantita)
+        {
+            AcquistoId = acquistoId;
+            DataAcquisto = dataAcquisto;
+            VideogiocoId = videogiocoId;
+            Quantita = quantita;
+        }
     }
 }
