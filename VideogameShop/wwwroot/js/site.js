0c9fcb76154d101e2495bc5ba1ecfb068592b3c7
RedGuardiaN7 <artiom.str@hotmail.it>
2023-02-03 15:02:12 +0100

    Hover prima versione

--- VideogameShop/wwwroot/js/site.js
+++ VideogameShop/wwwroot/js/site.js
@@ -1,1 +1,17 @@
-﻿
+﻿window.onload = async function afterWebPageLoad() {
+    await new Promise(r => setTimeout(r, 500));
+    const handleOnMouseMove = e => {
+        const { currentTarget: target } = e;
+
+        const rect = target.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+        target.style.setProperty("--mouse-x", `${x}px`);
+        target.style.setProperty("--mouse-y", `${y}px`);
+    }
+
+for (const user_card of document.querySelectorAll(".user_card")) {
+    user_card.onmousemove = e => handleOnMouseMove(e);
+}
+}
