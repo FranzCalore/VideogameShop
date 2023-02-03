60329a5855ff8c9c9cdefef523d60bcafd6944ea
ZahidRamirez00 <zahidramirez.00@gmail.com>
2023-02-03 10:45:06 +0100

    Implementando rifornimento fornitoreView

---
+++ VideogameShop/Models/ProvaView.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace VideogameShop.Models
+{
+    public class ProvaView
+    {
+        public RifornimentoFornitoreView RifornimentoFornitoreView { get; set; }
+        public VideogiocoTipologiaView VideogiocoTipologiaView { get; set; }
+    }
+}
--- VideogameShop/Models/RifornimentoFornitoreView.cs
+++ VideogameShop/Models/RifornimentoFornitoreView.cs
@@ -1,4 +1,4 @@
-﻿using VideogameShop.Models;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace VideogameShop.Models
 {
