d25d946c089d934760a6c32f74183556aa618b80
RedGuardiaN7 <artiom.str@hotmail.it>
2023-02-03 16:40:47 +0100

    Hover concluso, colori da discutere

--- VideogameShop/wwwroot/js/site.js
+++ VideogameShop/wwwroot/js/site.js
@@ -1,17 +1,14 @@
 ﻿window.onload = async function afterWebPageLoad() {
     await new Promise(r => setTimeout(r, 500));
-    const handleOnMouseMove = e => {
-        const { currentTarget: target } = e;
 
-        const rect = target.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
+    document.getElementById("user_cards").onmousemove = e => {
+        for (const card of document.getElementsByClassName("user_card")) {
+            const rect = card.getBoundingClientRect(),
+                x = e.clientX - rect.left,
+                y = e.clientY - rect.top;
 
-        target.style.setProperty("--mouse-x", `${x}px`);
-        target.style.setProperty("--mouse-y", `${y}px`);
+                card.style.setProperty("--mouse-x", `${x}px`);
+                card.style.setProperty("--mouse-y", `${y}px`);
+        }
     }
-
-for (const user_card of document.querySelectorAll(".user_card")) {
-    user_card.onmousemove = e => handleOnMouseMove(e);
-}
 }
