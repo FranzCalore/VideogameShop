69d01b82f5e5cf0d501bead432e79e41f185cf2b
FranzCalore <franzkaiser1993@gmail.com>
2023-02-09 16:19:46 +0100

    migrazione per fixare

---
+++ VideogameShop/Migrations/20230209151917_UtenteMessaggi.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VideogameShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class UtenteMessaggi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
